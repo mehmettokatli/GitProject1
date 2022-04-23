75d2b9909c5743fe4f01c59369712a9a71c49934
mehme <mehmet.tokatli999@gmail.com>
2022-04-23 14:52:57 +0300

    Added more work1

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("work1 started");
         System.out.println("Created Git Repository");
+        System.out.println("Made few commits");
+
 
 
 
